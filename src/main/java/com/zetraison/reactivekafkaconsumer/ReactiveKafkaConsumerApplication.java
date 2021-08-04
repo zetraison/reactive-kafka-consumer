8b76fe2bf9448b430bcf7af7ce495a9018599a1e
Alexandr Mozhaev <alexse790@mail.ru>
2021-08-04 21:55:07 +0500

    Initial commit

---
+++ src/main/java/com/zetraison/reactivekafkaconsumer/ReactiveKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.zetraison.reactivekafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveKafkaConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zetraison/reactivekafkaconsumer/ReactiveKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zetraison.reactivekafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
